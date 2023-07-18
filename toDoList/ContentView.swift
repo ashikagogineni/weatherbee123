458413f85e94136b76e64931ccd2410b1c5b07da
Ashika G <ashi.gogineni@gmail.com>
2023-07-18 15:02:44 -0700

    finished iteration 2

--- toDoList/ContentView.swift
+++ toDoList/ContentView.swift
@@ -8,42 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var context
     @State private var showNewTask = false
-    @State var toDoItems: [ToDoItem] = []
+    @FetchRequest(
+        entity: ToDo.entity(), sortDescriptors: [ NSSortDescriptor(keyPath: \ToDo.id, ascending: false) ])
+    
+    var toDoItems: FetchedResults<ToDo>
     
     var body: some View {
         VStack {
             HStack {
                 Text("To Do List")
-                     .font(.system(size: 40))
-                     .fontWeight(.black)
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
                 
                 Spacer()
                 Button(action: {
                     self.showNewTask = true
-                                    
+                    
                 }) {
-                Text("+")
+                    Text("+")
                 }
-            }// end of hstack
+            }
             .padding()
             Spacer()
             List {
-                    ForEach (toDoItems) { toDoItem in
-                        if toDoItem.isImportant == true {
-                            Text("‼️" + toDoItem.title)
-                        } else {
-                            Text(toDoItem.title)
+                ForEach (toDoItems) { toDoItem in
+                    if toDoItem.isImportant == true {
+                        Text("‼️" + (toDoItem.title ?? "No title"))
+                    } else {
+                        Text(toDoItem.title ?? "No title")
                     }
+                }
+                .onDelete(perform: deleteTask)
+            }
+            .listStyle(.plain)
+        }
+        if showNewTask {
+                NewToDoView(showNewTask: $showNewTask, title: "", isImportant: false)
+                    }
+        }
+    private func deleteTask(offsets: IndexSet) {
+            withAnimation {
+                offsets.map { toDoItems[$0] }.forEach(context.delete)
+
+                do {
+                    try context.save()
+                } catch {
+                    print(error)
                 }
             }
         }
-        if showNewTask {
-            NewToDoView(toDoItems: $toDoItems, showNewTask: $showNewTask, title: "", isImportant: false)
-                }
     }
-}
-
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
